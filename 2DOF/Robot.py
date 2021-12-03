6bde9d7bebae892496305453729fd7646e26bdce
mitsumaru3291 <morimitsu8428@gmail.com>
2021-12-03 16:01:31 +0900

    update Robot.py

--- 2DOF/Robot.py
+++ 2DOF/Robot.py
@@ -7,9 +7,12 @@ class Robot:
         self.q1 = q1
         self.q2 = q2
 
+    def set(self, q1, q2):
+        self.q1 = q1
+        self.q2 = q2
+
     def get_joint1_position(self):
-        return [self.l1*math.cos(self.q1), self.l1*math.sin(self.q1)]
+        return self.l1*math.cos(self.q1), self.l1*math.sin(self.q1)
 
     def get_joint2_position(self):
-        return [self.l1*math.cos(self.q1) + self.l2*math.cos(self.q1 + self.q2),
-                self.l1*math.sin(self.q1) + self.l2*math.sin(self.q1 + self.q2)]
+        return self.l1*math.cos(self.q1) + self.l2*math.cos(self.q1 + self.q2), self.l1*math.sin(self.q1) + self.l2*math.sin(self.q1 + self.q2)
